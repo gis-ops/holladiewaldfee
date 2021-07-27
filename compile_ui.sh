cd520898171aed7780e0cdb25ac16688690e1a43
Christian Beiwinkel <c.beiwinkel@students.uu.nl>
2021-07-27 07:28:19 +0200

    waypoint widget integrated

--- compile_ui.sh
+++ compile_ui.sh
@@ -2,7 +2,10 @@
 for f in qrouting/ui/raw/*
 do
   file_base=$(basename "$f")
-  py_f=${file_base%%.*}
-  echo "compiled $py_f"
-  pyuic5  "$f" > qrouting/ui/"${py_f}_ui.py"
+  if [ "$file_base" != qrouting_dialog_base.ui ]
+  then
+    py_f=${file_base%%.*}
+    echo "compiled $py_f"
+    pyuic5  "$f" > qrouting/ui/"${py_f}_ui.py"
+  fi
 done
